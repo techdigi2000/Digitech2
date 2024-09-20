262f590b2db131cfb31aeead443f49e150754822
techdigi2000 <techdigi307@gmail.com>
2024-09-20 16:59:45 +0530

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,21 +8,13 @@ import SpecialOffers from "../../components/home/SpecialOffers/SpecialOffers";
 import YearProduct from "../../components/home/YearProduct/YearProduct";
 import BlueNavbar from "../../components/BlueNavBar/BlueNav";
 import WirelessPrinterSetup from "../../components/PrinterSetup/PrinterSetup";
+import Setup from "../Setup/Setup";
 
 const Home = () => {
   return (
     <div className="w-full mx-auto">
-      <BlueNavbar />
-      <Banner />
-      <Sale />
-      <BannerBottom />
-      <div className="max-w-container mx-auto px-4">
-        <NewArrivals />
-        <BestSellers />
-        <YearProduct />
-        <SpecialOffers />
-        <WirelessPrinterSetup />
-      </div>
+      <Setup />
+      <WirelessPrinterSetup />
     </div>
   );
 };
